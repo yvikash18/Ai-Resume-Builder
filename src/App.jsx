bca6c1eb0624a47eb78dbbc3e2144cced4cdde3f
Vikash Yadav <yvikash880@gmail.com>
2024-09-16 12:34:57 -0700

    Portfolio update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import { ButtonIcon } from '@radix-ui/react-icons'
 import { Button } from './components/ui/button'
 import { Navigate, Outlet } from 'react-router-dom'
 import { useUser } from '@clerk/clerk-react'
-import Header from './components/custom/header'
+import Header from "./components/custom/header.jsx";
+
 
 function App() {
   const [count, setCount] = useState(0)
