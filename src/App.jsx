0882cc0ff9a00e8014ecfb0e19ba56b8ced211a1
Vikash Yadav <yvikash880@gmail.com>
2024-09-16 11:33:53 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { ButtonIcon } from '@radix-ui/react-icons'
+import { Button } from './components/ui/button'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useUser } from '@clerk/clerk-react'
 
 function App() {
   const [count, setCount] = useState(0)
-
-  return (
+ const {user,isLoaded,isSignedIn}=useUser();
+ if (!isSignedIn){
+  return <Navigate to={'/auth/sign-in'}/>
+ }
+   return (
     <>
-     Subcribe to my Channel
-     <ButtonIcon/>
+   <Outlet/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,39 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignInPage from './auth/sign-in/index.jsx'
+import Home from './home/index.jsx'
+import Dashboard from './dashboard/index.jsx'
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+import { ClerkProvider } from '@clerk/clerk-react'
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const router=createBrowserRouter([
+  {
 
+    element:<App/>,
+    children:[{
+    path:"/",
+    element:<Home/>
+    },
+    {
+      path:"/dashboard",
+      element:<Dashboard/>
+    }
+  ],
+  },
+  {
+    path:"/auth/sign-in",
+    element:<SignInPage/>
+  }
+]
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider  publishableKey={PUBLISHABLE_KEY}>
+   <RouterProvider router = {router}/>
+    </ClerkProvider>
   </StrictMode>,
 )
