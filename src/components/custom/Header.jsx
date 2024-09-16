dccba2466db33738690b6615d84b7d5142b3a77e
Vikash Yadav <yvikash880@gmail.com>
2024-09-16 12:26:54 -0700

    update

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 export default function Header() {
   return (
-    <div>
+    <div className='p-3 px-5 flex justify-between shadow-md'>
         <img src="/logo.svg" width={100} height={100}/>
+        <Button>Get Started</Button>
     </div>
   )
 }
--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,12 +1,10 @@
 import Header from '@/components/custom/header'
-import { UserButton } from '@clerk/clerk-react'
 import React from 'react'
 
 export default function Home() {
   return (
     <div>
-     <Header/> 
-     LandingScreen
+      <Header/>
     </div>
   )
 }
